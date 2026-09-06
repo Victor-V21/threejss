f365fe2b004a2cbf7cc2dca2bf4c4ec82c366319
Victor-V21 <vegav2605@gmail.com>
2026-09-06 15:12:15 -0600

    feat: enhance PersonModel component with animation playback and shadow settings

--- src/animation/PersonModel.tsx
+++ src/animation/PersonModel.tsx
@@ -1,21 +1,35 @@
 // este componente carga un modelo 3D
 
-import { useGLTF } from "@react-three/drei"
+import { useAnimations, useGLTF } from "@react-three/drei";
+import { useEffect, useRef } from "react";
 
-export const PersonModel = (props:any) => {
+export const PersonModel = (props: any) => {
+  // referencia del modelo
+  const modelRef = useRef(null);
 
-    const gltf = useGLTF("/models/person.glb")
+  // cargamos el modelo
+  const gltf = useGLTF("/models/person.glb");
 
-    // se recorren todas las piezas del modelo
-    gltf.scene.traverse((child:any) =>{
+  // extraemos las acciones y animaciones del modelo
+  const { actions, names } = useAnimations(gltf.animations, modelRef);
 
-        if (child.isMesh) {
-            child.castShadow = true
-            child.receiveShadow = true
-        }
-    })
+  // usamos useEffect para imprimir los nombres en la consola
+  useEffect(() => {
+    // mostramos las animaciones disponibles del modelo en la consola
+    console.log("Animaciones disponibles: ", names);
 
-  return (
-    <primitive object={gltf.scene} {...props} />
-  )
-}
+    // ejecutamos la animación que vimos en la consola
+    actions["Animation"]?.play();
+  }, [names, actions]);
+
+  // se recorren todas las piezas del modelo
+  gltf.scene.traverse((child: any) => {
+    // por cada hijo del modelo calculamos su sombra y recibo de luz por individual
+    if (child.isMesh) {
+      child.castShadow = true;
+      child.receiveShadow = true;
+    }
+  });
+
+  return <primitive object={gltf.scene} {...props} ref={modelRef} />;
+};
